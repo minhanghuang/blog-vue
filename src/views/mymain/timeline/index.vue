fd3eb2fdd7d993112d91c090ac8f72168897de10
Coxhuang <mhesat@outlook.com>
2019-12-11 23:32:52 +0800

    update

--- src/views/mymain/timeline/index.vue
+++ src/views/mymain/timeline/index.vue
@@ -3,9 +3,30 @@
 </style>
 
 <template>
-	<Row style="height: 100%;width: 100%;">
+	<Row style="height: 100%;width: 100%;min-height: 700px;background-color: white">
 		<Col style="height: 100%;width: 100%;">
-			aaa
+			<div class="block">
+				<el-timeline>
+					<el-timeline-item timestamp="2018/4/12" placement="top">
+						<el-card>
+							<h4>更新 Github 模板</h4>
+							<p>王小虎 提交于 2018/4/12 20:46</p>
+						</el-card>
+					</el-timeline-item>
+					<el-timeline-item timestamp="2018/4/3" placement="top">
+						<el-card>
+							<h4>更新 Github 模板</h4>
+							<p>王小虎 提交于 2018/4/3 20:46</p>
+						</el-card>
+					</el-timeline-item>
+					<el-timeline-item timestamp="2018/4/2" placement="top">
+						<el-card>
+							<h4>更新 Github 模板</h4>
+							<p>王小虎 提交于 2018/4/2 20:46</p>
+						</el-card>
+					</el-timeline-item>
+				</el-timeline>
+			</div>
 		</Col>
 	</Row>
 </template>
